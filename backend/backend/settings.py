fc265c4f036204a468a04d4e2a71a872cc247602
Pranav Krishna <pranavkrishna404@gmail.com>
2026-03-31 14:42:37 +0530

    Frontend form creation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1w=ne%)0qjxtn#_v-%yrt$tn@n%qj&08jtfe=*icj#is!x^c)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ frontend/src/signup.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const Signup = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('api/auth/register/', { name:name, email:email, password:password });
+        } catch (error) {
+            console.error('Error signing up:', error.response.data);
+        }
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <button type="submit">Sign Up</button>
+        </form>
+    )
+
+}
+
+export default Signup;
+
