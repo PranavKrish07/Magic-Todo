85e56805e4ea9dbc97c07ded50b3507b89b9720a
Pranav Krishna <pranavkrishna404@gmail.com>
2026-04-14 13:25:55 +0530

    Cors origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,7 +66,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://magic-todo-rouge.vercel.app",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://magic-todo-rouge.vercel.app",
+    "https://magic-todo-mp8a.onrender.com",
+]
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
